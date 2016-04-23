d7756cfb76e92447af00d8b7d0eeb388ad392250
Stephen Grice <stephengrice@live.com>
2016-04-23 13:01:05 -0400

    Fix wrong domain in deploy script

--- deploy/wow.php
+++ deploy/wow.php
@@ -1,6 +1,6 @@
 <?php
 echo 'Moving to proper directory <br/>';
-echo shell_exec('cd ~/public_html/domains/njjunkcar.net');
+echo shell_exec('cd ~/public_html/domains/haynj.com');
 echo 'Attempting git pull <br/>';
 echo shell_exec('git pull origin master') . '<br/>';
 echo 'Terminating deploy script';
